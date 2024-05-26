3fbd2026cc45481cb7c5514134088f808c382329
SandeepTimalsina <9807173999a@gmail.com>
2024-05-26 15:14:02 +0545

    fixed a error

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Admin(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,6 +10,7 @@ class Admin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
 
 class Staffs(models.Model):
     id = models.AutoField(primary_key=True)
@@ -20,6 +22,7 @@ class Staffs(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
+
 class Courses(models.Model):
     id = models.AutoField(primary_key=True)
     course_name = models.CharField(max_length=255)
@@ -27,12 +30,14 @@ class Courses(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
 
+
 class Subjects(models.Model):
     id = models.AutoField(primary_key=True)
-    staff_id = models.ForeignKey(Staffs, on_delete = models.CASCADE)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
     subject = models.CharField(max_length=255)
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     objects = models.Manager()
+
 
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
@@ -43,40 +48,43 @@ class Students(models.Model):
     profile_pic = models.FileField()
     address = models.TextField()
     objects = models.Manager()
-    course_id = models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
+    course_id = models.ForeignKey(Courses, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    
+
 class Attendance(models.Model):
     id = models.AutoField(primary_key=True)
-    subject_id = models.ForeignKey(Subjects,on_delete=models.DO_NOTHING)
+    subject_id = models.ForeignKey(Subjects, on_delete=models.DO_NOTHING)
     attendance_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
+
 class AttendenceReport(models.Model):
     id = models.AutoField(primary_key=True)
-    attendance_id = models.ForeignKey(Attendance,on_delete=models.DO_NOTHING)
-    student_id = models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    attendance_id = models.ForeignKey(Attendance, on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(Students, on_delete=models.DO_NOTHING)
     status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
+
 class LeaveReportStudent(models.Model):
     id = models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(Students, on_delete=models.DO_NOTHING)
     leave_date = models.CharField(max_length=255)
     leave_message = models.TextField()
     leave_status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
 
 class LeaveReportStaff(models.Model):
     id = models.AutoField(primary_key=True)
-    staff = models.ForeignKey(Staffs,on_delete=models.DO_NOTHING)
+    staff = models.ForeignKey(Staffs, on_delete=models.DO_NOTHING)
     leave_date = models.CharField(max_length=255)
     leave_message = models.TextField()
     leave_status = models.BooleanField(default=False)
@@ -84,39 +92,40 @@ class LeaveReportStaff(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
+
 class FeedBackStudent(models.Model):
     id = models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(Students, on_delete=models.DO_NOTHING)
     feedback = models.TextField()
     feedback_reply = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
 
 class FeedBackStaffs(models.Model):
     id = models.AutoField(primary_key=True)
-    staff_id = models.ForeignKey(Staffs,on_delete=models.DO_NOTHING)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.DO_NOTHING)
     feedback = models.TextField()
     feedback_reply = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
+
 class NotificationsStudent(models.Model):
     id = models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(Students, on_delete=models.DO_NOTHING)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
 
 class NotificationsStaffs(models.Model):
     id = models.AutoField(primary_key=True)
-    staff_id = models.ForeignKey(Staffs,on_delete=models.DO_NOTHING)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.DO_NOTHING)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
-
-
-
